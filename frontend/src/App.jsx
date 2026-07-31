6e774df66a522b2dfb34a9713952f99c360e77cd
yaghmourassen <wiixou@gmail.com>
2026-07-31 15:06:59 +0200

    fix: rename Home.jsx and update App.jsx import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, lazy, Suspense } from 'react';
 import SEO from './components/SEO';
-import Home from './pages/home'; 
+import Home from './pages/Home'; 
 import Login from './pages/login';
 import './App.css'; 
 
