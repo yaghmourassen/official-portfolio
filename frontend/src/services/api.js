34cfc8d6877be7080b947fd402c7d17121d32097
yaghmourassen <wiixou@gmail.com>
2026-07-31 17:18:11 +0200

    feat: use environment variable for API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,5 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api` 
+  : "http://localhost:5000/api";
 
 export default API_URL;
