8dbedbae9ad4ac06ed6276b77e2e1386300aa142
yaghmourassen <wiixou@gmail.com>
2026-08-01 11:32:17 +0200

    fix(ui): solve mobile navbar toggle state and eliminate horizontal overflow in hero section

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async' // 1. استيراد الموفر
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './index.css'
 import App from './App.jsx'
 
