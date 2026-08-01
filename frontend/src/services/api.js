7e5ead6062a8ac7967b4e017dfa5dc8578934005
yaghmourassen <wiixou@gmail.com>
2026-08-01 12:01:17 +0200

    api printinmg from consol

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
 const API_URL = import.meta.env.VITE_API_URL 
   ? `${import.meta.env.VITE_API_URL}/api` 
   : "http://localhost:5000/api";
+  console.log("API_URL =", API_URL);
+
 
 export default API_URL;
